fe30db03827b6fece15032e894f6d862bdf2d209
EliGolam <elistasibusiness@gmail.com>
2022-09-22 11:43:11 +0200

    First Vue Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld userName="Elias" />
+    <FirstComponent />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import FirstComponent from "./components/first-component.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    FirstComponent,
   },
 };
 </script>
---
+++ src/components/first-component.vue
@@ -0,0 +1,74 @@
+<template>
+<div>
+    <div 
+        class="generic"
+        :class="{ active: activeClass}" 
+        @click="sayHi">I'm Generic
+        <input v-model="text" class="inner" type="text" placeholder="I'm Inner within Generic" />
+    
+        <div class="box-1 inner">Box 1</div>
+        <div class="box-2">Box 2</div>
+    </div>
+    <input v-model="text" class="inner" type="text" placeholder="I'm Inner Outside of Generic" />
+    
+    
+</div>
+</template>
+
+
+<script>
+export default {
+    name: "FirstComponent",
+
+    data() {
+        return {
+            activeClass: false,
+        }
+    },
+
+    methods: {
+        sayHi() {
+            console.log("Hello!");
+            this.activeClass = !this.activeClass;
+        }
+    }
+} 
+</script>
+
+<style lang="scss" scoped>
+$new-clr: aquamarine;
+$another-clr: yellow;
+$more-clr: lightblue;
+
+@mixin ac-square {
+    width: 100px;
+    aspect-ratio: 1 / 1;
+    margin: 1rem auto;
+    border-radius: .7em;
+}
+
+.generic {
+    background-color: $another-clr;
+
+    .inner {
+        background-color: $more-clr;
+    }
+
+    &.active {
+        background-color: $new-clr;
+    }
+}
+
+.inner {
+    background-color: whitesmoke;
+}
+
+.box-1 {
+    @include ac-square();
+}
+
+.box-2 {
+    @include ac-square();
+    background-color: aquamarine;
+}
+</style>
